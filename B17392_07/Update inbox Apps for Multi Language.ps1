a5b189708f57220e1f6b7e5643d41809f3d3e767
RMITBLOG <48697709+RMITBLOG@users.noreply.github.com>
2021-07-26 12:50:03 +0100

    Update Inbox Apps for Multi Language

---
+++ B17392_07/Update inbox Apps for Multi Language.ps1
@@ -0,0 +1,27 @@
+﻿#########################################
+## Update Inbox Apps for Multi Language##
+#########################################
+##Set Inbox App Package Content Stores##
+[string] $AppsContent = "F:\"
+//
+##Update installed Inbox Store Apps##
+foreach ($App in (Get-AppxProvisionedPackage -Online)) {
+	$AppPath = $AppsContent + $App.DisplayName + '_' + $App.PublisherId
+	Write-Host "Handling $AppPath"
+	$licFile = Get-Item $AppPath*.xml
+	if ($licFile.Count) {
+		$lic = $true
+		$licFilePath = $licFile.FullName
+	} else {
+		$lic = $false
+	}
+	$appxFile = Get-Item $AppPath*.appx*
+	if ($appxFile.Count) {
+		$appxFilePath = $appxFile.FullName
+		if ($lic) {
+			Add-AppxProvisionedPackage -Online -PackagePath $appxFilePath -LicensePath $licFilePath 
+		} else {
+			Add-AppxProvisionedPackage -Online -PackagePath $appxFilePath -skiplicense
+		}
+	}
+}
